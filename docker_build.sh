25e4db3349d849424b456eb36595e39b11772fc0
Reza Farrahi <reza.farrahi@nakisa.com>
2018-08-14 10:45:39 -0400

    minor fix

--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,4 @@
 # 180807 Reza Farrahi (imriss@yahoo.com)
 
 #
-rm master.tar.gz*; wget https://github.com/imriss/docker-squid/archive/master.tar.gz; tar -xvf master.tar.gz;
-# tar -xvf master.tar.gz docker-squid-master/Dockerfile
-
-# docker build --squash -t imriss/docker-squid .
-cd docker-squid-master/; ./docker_build.sh; cd ..; rm -rd docker-squid-master; rm master.tar.gz*;
+docker build --squash -t imriss/docker-proxy .
